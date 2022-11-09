b5513920d6e4d63a0ba69b1a2eaf373365825a23
gabrielsapp <ogabrielsapp@gmail.com>
2022-11-09 15:26:11 -0300

    ex 01 concluido

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,21 @@
 const jogada1 = "pedra"
 const jogada2 = "tesoura"
 
+
+
 //seu código aqui
+if(jogada1 == "tesoura" && jogada2 == "pedra"){
+    console.log('pedra');
+}else if(jogada1 == "pedra" && jogada2 == "tesoura"){
+    console.log('pedra');
+}else if(jogada1 == "pedra" && jogada2 == "papel"){
+    console.log('papel');
+}else if(jogada1 == "papel" && jogada2 == "tesoura"){
+    console.log('tesoura');
+}else if(jogada1 == "tesoura" && jogada2 == "papel"){
+    console.log('tesoura');
+}else if(jogada1 == "papel" && jogada2 == "pedra"){
+    console.log('tesoura');
+}else if(jogada1 == jogada2){
+    console.log('empate');
+}
